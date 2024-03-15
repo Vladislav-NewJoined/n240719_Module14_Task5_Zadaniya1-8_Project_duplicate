4441274bb9727d9d7109c461d401a8ba93ad777a
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-03-15 16:42:14 +0300

    Initial Commit

---
+++ src/task4_6_1/Task4_6_1.java
@@ -0,0 +1,16 @@
+package task4_6_1;
+
+import java.io.IOException;
+
+public class Task4_6_1 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("""
+                Задание:\s
+                Модуль 4. Наследование. Задание №6:\s
+                    1. Доработайте крестики-нолики; создайте исключение, которое будете бросать при неверном
+                       вводе пользователя\s
+
+                Решение:\s""");
+
+    }
+}
