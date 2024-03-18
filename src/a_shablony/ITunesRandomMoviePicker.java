57170eea0c0d421587b3da861958fba620cfaf81
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-03-18 08:41:23 +0300

    Initial Commit

--- src/a_shablony/ITunesRandomMoviePicker.java
+++ src/a_shablony/ITunesRandomMoviePicker.java
@@ -2,9 +2,23 @@ package a_shablony;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class ITunesRandomMoviePicker {
     public static void main(String[] args) throws IOException {
+
+
+
+
+
+
+
+
+
 //        RandomMoviePicker moviePicker = new RandomMoviePicker();
         RandomMoviePicker moviePicker = new RandomMoviePicker();
         Movie[] movies = moviePicker.getRandomMovieNames();
@@ -22,11 +36,17 @@ public class ITunesRandomMoviePicker {
     }
 }
 /*public */class RandomMoviePicker {
-    PageDownloader downloader = new PageDownloader();
+    PageDownloader2 downloader2 = new PageDownloader2();
 
     Movie[] getRandomMovieNames() {
-        String url = "https://randommer.io/random-movies";
-        String page = downloader.downloadWebPage(url);
+        String url = "https://ya.ru";
+
+
+
+
+
+//        String url = "https://randommer.io/random-movies";
+        String page = downloader2.downloadWebPage(url);
 
         Movie[] movies = new Movie[16];
         int endIndex = 0;
@@ -44,16 +64,97 @@ public class ITunesRandomMoviePicker {
     }
 }
 
-    /*private */class Movie {
+/*private */class Movie {
 
-        public int year;
-        public int name;
+    public int year;
+    public int name;
 
-        public Movie(String nameWithoutYear, String year) {
+    public Movie(String nameWithoutYear, String year) {
 
-        }
     }
+}
 
+/*public */class PageDownloader2 {
+    public String downloadWebPage(String url) /*throws IOException */{
+        StringBuilder result = new StringBuilder();
+        String line;
+        URLConnection urlConnection = null;
+        try {
+            urlConnection = new URL(url).openConnection();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (InputStream is = urlConnection.getInputStream();
+             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            while ((line = br.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return result.toString(); // goto 13 line
+    }
+}
+
+
+
+
+
+//// ПРИМЕР 3
+//import java.io.IOException;
+//import java.util.Scanner;
+//
+//public class ITunesRandomMoviePicker {
+//    public static void main(String[] args) throws IOException {
+////        RandomMoviePicker moviePicker = new RandomMoviePicker();
+//        RandomMoviePicker moviePicker = new RandomMoviePicker();
+//        Movie[] movies = moviePicker.getRandomMovieNames();
+//        System.out.println("Chose film to watch: ");
+//        for (int i = 0; i < movies.length; i++) {
+//            int number = i + 1;
+//            System.out.println(number + ": " + movies[i].name + "(" + movies[i].year + ")");
+//        }
+//        System.out.print("Film number: ");
+//        Scanner scanner = new Scanner(System.in);
+//        int chosenNumber = scanner.nextInt();
+//        int chosenIndex = chosenNumber - 1;
+//        ITunesMoviePlayer player = new ITunesMoviePlayer();
+//        player.playMovie(String.valueOf(movies[chosenIndex].name));
+//    }
+//}
+///*public */class RandomMoviePicker {
+//    PageDownloader2 downloader2 = new PageDownloader2();
+//
+//    Movie[] getRandomMovieNames() {
+//        String url = "https://randommer.io/random-movies";
+//        String page = downloader2.downloadWebPage(url);
+//
+//        Movie[] movies = new Movie[16];
+//        int endIndex = 0;
+//
+//        for (int i = 0; i < 16; i++) {
+//            int captionIndex = page.indexOf("<div class=caption", endIndex);
+//            int startIndex = captionIndex + 22;
+//            endIndex = page.indexOf("</div>", startIndex);
+//            String movieName = page.substring(startIndex, endIndex);
+//            String nameWithoutYear = movieName.substring(0, movieName.length() - 6);
+//            String year = movieName.substring(movieName.length() - 5, movieName.length() - 1);
+//            movies[i] = new Movie(nameWithoutYear, year);
+//        }
+//        return movies;
+//    }
+//}
+//
+///*private */class Movie {
+//
+//    public int year;
+//    public int name;
+//
+//    public Movie(String nameWithoutYear, String year) {
+//
+//    }
+//}
+//// КОНЕЦ ПРИМЕРА 3
 
 
 
@@ -66,12 +167,12 @@ public class ITunesRandomMoviePicker {
 ////    }
 ////    // конец источника: https://www.javatpoint.com/how-to-open-a-file-in-java
 //
-//    a_shablony.PageDownloader downloader = new a_shablony.PageDownloader();
+//    a_shablony.PageDownloader2 downloader2 = new a_shablony.PageDownloader2();
 //
 //    void playMovie(String searchRequest) throws IOException {
 //        String url = buildUrl(searchRequest);
 //        System.out.println("Will search film by term: " + searchRequest);
-//        String page = downloader.downloadWebPage(url);
+//        String page = downloader2.downloadWebPage(url);
 //
 //        String movieName = getTag(page, "trackName");
 //        String previewUrl = getTag(page, "previewUrl");
---
+++ src/task4_7_1/Task4_7_1.java
@@ -0,0 +1,13 @@
+package task4_7_1;
+
+public class Task4_7_1 {
+    public static void main(String[] args) {
+        System.out.println("""
+                Задание:\s
+                Модуль 4. Наследование. Задание №7:\s
+                    1. В чем разница между throw и throws в Java?\s
+
+                Решение:\s""");
+
+    }
+}
