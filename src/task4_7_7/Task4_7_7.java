5865ef16b0d9a55f449237eaa74283c5ccfbab28
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-03-18 20:24:38 +0300

    Initial Commit

---
+++ src/task4_7_7/Task4_7_7.java
@@ -0,0 +1,14 @@
+package task4_7_7;
+
+public class Task4_7_7 {
+    public static void main(String[] args) {
+        System.out.println("""
+                Задание:\s
+                Модуль 4. Наследование. Задание №7:\s
+                    7. Доработайте скачивальщик снимков NASA, чтобы в нем не было ни одного throws
+                       (пройдитесь прям поиском по файлу). Throws придётся заменить на try catch.\s
+
+                Решение:\s""");
+
+    }
+}
