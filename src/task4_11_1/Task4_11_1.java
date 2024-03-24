23af06daed23f59f28fd19b3f6f0e0e235a08123
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-03-24 17:44:59 +0300

    Initial Commit

---
+++ src/task4_11_1/Task4_11_1.java
@@ -0,0 +1,13 @@
+package task4_11_1;
+
+public class Task4_11_1 {
+    public static void main(String[] args) {
+        System.out.println("""
+                Задание:\s
+                Модуль 4. Наследование. Задание №11:\s
+                    1. Что такое stacktrace?\s
+
+                Решение:\s""");
+
+    }
+}
