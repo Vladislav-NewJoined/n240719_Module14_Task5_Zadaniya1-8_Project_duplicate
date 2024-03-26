2f9f29b4ce185515a046d408c53683c0d000f91d
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-03-26 20:28:04 +0300

    Initial Commit

--- src/task5_2_1/Task5_2_1.java
+++ src/task5_2_1/Task5_2_1.java
@@ -39,3 +39,48 @@ class FileInformation {
         this.size = inputFile.length();
     }
 }
+
+
+
+
+//// ПРИМЕР 2
+//import java.io.File;
+//
+//public class Task5_2_1 {
+//    public static void main(String[] args) {
+//        System.out.println("""
+//                Задание:\s
+//                Модуль 5. Интерфейсы, абстрактные классы, статические методы. Задание №2:\s
+//                    1. У класса FileInformation из предыдущих уроков сделайте метод toString, возвращающий
+//                       информацию по всем полям.\s
+//
+//                Решение:\s""");
+//
+//        FileInformation fileInformation = new FileInformation("text.txt", "src\\task5_2_1", 1000);
+////        fileInformation.init("text.txt", "src\\task5_2_1", 1000);
+//        fileInformation.fileName = "text.txt";
+//        fileInformation.size = 1000;
+//        fileInformation.absolutePath = "src\\task5_2_1";
+//
+//    }
+//}
+//
+//class FileInformation {
+//    String fileName;
+//    String absolutePath;
+//    long size;
+//
+//    //    void init(String inputFileName, String inputAbsolutePath, long inputSize) {
+//    FileInformation(String inputFileName, String inputAbsolutePath, long inputSize) {
+//        this.fileName = inputFileName;
+//        this.absolutePath = inputAbsolutePath;
+//        this.size = inputSize;
+//    }
+//
+//    FileInformation(File inputFile) {
+//        this.absolutePath = inputFile.getAbsolutePath();
+//        this.fileName = inputFile.getName();
+//        this.size = inputFile.length();
+//    }
+//}
+//// КОНЕЦ ПРИМЕРА 2
