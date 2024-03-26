32a3f066aaddc66eeafe72e6205ddb641f872367
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-03-26 19:40:31 +0300

    Initial Commit

---
+++ src/task5_2_1/Task5_2_1.java
@@ -0,0 +1,14 @@
+package task5_2_1;
+
+public class Task5_2_1 {
+    public static void main(String[] args) {
+        System.out.println("""
+                Задание:\s
+                Модуль 5. Интерфейсы, абстрактные классы, статические методы. Задание №2:\s
+                    1. У класса FileInformation из предыдущих уроков сделайте метод toString, возвращающий
+                       информацию по всем полям.\s
+
+                Решение:\s""");
+
+    }
+}
