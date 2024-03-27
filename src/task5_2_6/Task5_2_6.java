5eede32403eb165db7729fab15108ac79c020176
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-03-27 13:58:47 +0300

    Initial Commit

---
+++ src/task5_2_6/Task5_2_6.java
@@ -0,0 +1,19 @@
+package task5_2_6;
+
+import java.io.IOException;
+
+public class Task5_2_6 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("""
+                Задание:\s
+                Модуль 5. Интерфейсы, абстрактные классы, статические методы. Задание №2:\s
+                    6. Сделайте метод toString у класса ITunesSong, который возвращает информацию в
+                       формате xml: <artist>Rick Ross</artist>… и так далее\s
+
+                Решение:\s""");
+
+    }
+}
+
+
+
---
+++ src/task5_2_7/Task5_2_7.java
@@ -0,0 +1,19 @@
+package task5_2_7;
+
+import java.io.IOException;
+
+public class Task5_2_7 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("""
+                Задание:\s
+                Модуль 5. Интерфейсы, абстрактные классы, статические методы. Задание №2:\s
+                    7. Сделайте метод toString у класса ITunesSong, который возвращает информацию
+                       в формате JSON\s
+
+                Решение:\s""");
+
+    }
+}
+
+
+
