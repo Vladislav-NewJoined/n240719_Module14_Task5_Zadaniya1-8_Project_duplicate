e9fe7cab5aabba6ebb20ae6c3c0fe0c044cac1e9
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-03-29 14:30:41 +0300

    Initial Commit

---
+++ src/task5_3_9/Task5_3_9.java
@@ -0,0 +1,15 @@
+package task5_3_9;
+
+import java.io.IOException;
+
+public class Task5_3_9 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("""
+                Задание:\s
+                Модуль 5. Интерфейсы, абстрактные классы, статические методы. Задание №3:\s
+                    9. Реализуйте метод equals для класса NasaPictureInfo. Как сделать его максимально просто?\s
+
+                Решение:\s""");
+
+    }
+}
