9cff4b7c2751c8279375351efd7ed561765f5ef6
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-04-01 16:10:26 +0300

    Initial Commit

---
+++ src/task5_5_0_Videourok/Task5_5_0_Videourok.java
@@ -0,0 +1,17 @@
+package task5_5_0_Videourok;
+
+import java.io.IOException;
+
+public class Task5_5_0_Videourok {
+    public static void main(String[] args) throws IOException {
+        System.out.println("""
+                Задание:\s
+                Модуль 5. Интерфейсы, абстрактные классы, статические методы. Задание №5:\s
+                    1. Создайте абстрактный класс SongLyrics, который возвращает текст песни по названию.\s
+
+                Решение:\s""");
+
+        System.out.println("""
+                Some text.\s""");
+    }
+}
