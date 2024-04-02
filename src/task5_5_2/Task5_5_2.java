f4adbefb62d604b6990d9065d3aaa66b3089c7cb
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-04-02 15:07:03 +0300

    Initial Commit

--- src/task5_5_2/Task5_5_2.java
+++ src/task5_5_2/Task5_5_2.java
@@ -1,10 +1,8 @@
 package task5_5_2;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 abstract class SongLyrics {
     protected abstract String getLyrics(String song);
@@ -15,103 +13,53 @@ public class Task5_5_2 {
         System.out.println("""
                 Задание:\s
                 Модуль 5. Интерфейсы, абстрактные классы, статические методы. Задание №5:\s
-                    2. Создайте файл в формате: Название песни: текст песни, и так много песен (можете
-                       разделять разные песни пустыми строками, или другим способом) Создайте
-                       наследника - FileSongLyrics, который ищет в файле по названию песни, и если
-                       находит, возвращает текст песни.\s
+                    2. Создайте файл в формате: Название песни: текст песни, и так много песен
+                       (можете разделять разные песни пустыми строками, или другим способом)
+                       Создайте наследника - FileSongLyrics, который ищет в файле по названию
+                        песни, и если находит, возвращает текст песни.\s
 
                 Решение:\s""");
+        System.out.println("В файл src/task5_5_2/SongLyrics.txt в тестовом режиме записаны" +
+                " названия и тексты трёх песен. Ищем в файле песни по названию.\n");
+        FileSongLyrics fileSongLyrics = new FileSongLyrics();
+        String song = "Life is life";
 
-        LyricsDownloader lyricsDownloader = new LyricsDownloader();
-        String song = "Lose yourself";
-
-        String lyrics = lyricsDownloader.getLyrics(song);
-        System.out.println("Выводим текст песни '" + song + "', исполнителя 'Eminem':\n" + lyrics);
+        String lyrics = fileSongLyrics.getLyrics(song);
+        System.out.println("Выводим текст песни '" + song + "':\n" + lyrics);
     }
 }
 
-class LyricsDownloader extends SongLyrics {
-
+class FileSongLyrics extends SongLyrics {
     @Override
     protected String getLyrics(String song) {
-        StringBuilder response = new StringBuilder();
+        StringBuilder lyrics = new StringBuilder();
+        boolean foundSong = false;
 
-        try {
-            HttpURLConnection connection = (HttpURLConnection) new URL(getURL("Eminem", song)).openConnection();
-            connection.setRequestMethod("GET");
-
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
+        try (BufferedReader br = new BufferedReader(new FileReader("src/task5_5_2/SongLyrics.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.trim().equals(song)) {
+                    foundSong = true;
+                    continue;
+                }
+                if (foundSong) {
+                    if (line.trim().isEmpty()) {
+                        break;
+                    }
+                    lyrics.append(line);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        String lyrics = response.toString().split("\"lyrics\":\"")[1].split("\"")[0];
-        return lyrics;
-    }
-
-    private String getURL(String artist, String song) {
-        return "https://api.lyrics.ovh/v1/" + artist + "/" + song.replace(" ", "%20");
+        if (foundSong) {
+            return lyrics.toString();
+        } else {
+            return "Песня не найдена";
+        }
     }
 }
-
-
-
-
-//// ПРИМЕР 3
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.net.HttpURLConnection;
-//import java.net.URL;
-//
-//public class Task5_5_2 {
-//    public static void main(String[] args) throws IOException {
-//        System.out.println("""
-//                Задание:\s
-//                Модуль 5. Интерфейсы, абстрактные классы, статические методы. Задание №5:\s
-//                    1. Создайте абстрактный класс SongLyrics, который возвращает текст песни
-//                       по названию.\s
-//
-//                Решение:\s""");
-//
-//        LyricsDownloader lyricsDownloader = new LyricsDownloader();
-//        String artist = "Eminem";
-//        String song = "Lose yourself";
-//
-//        try {
-//            String lyrics = lyricsDownloader.downloadLyrics(artist, song);
-//            System.out.println("Текст песни '" + song + "', исполнителя '" + artist + "':\n" + lyrics);
-//        } catch (IOException e) {
-//            System.out.println("Error downloading lyrics: " + e.getMessage());
-//        }
-//    }
-//}
-//
-//class LyricsDownloader {
-//    public static String downloadLyrics(String artist, String song) throws IOException {
-//        String url = "https://api.lyrics.ovh/v1/" + artist + "/" + song.replace(" ", "%20");
-//        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-//        connection.setRequestMethod("GET");
-//
-//        StringBuilder response = new StringBuilder();
-//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                response.append(line);
-//            }
-//        }
-//
-//        // Обработка JSON ответа, чтобы получить текст песни
-//        String lyrics = response.toString().split("\"lyrics\":\"")[1].split("\"")[0];
-//        return lyrics;
-//    }
-//}
-//// КОНЕЦ ПРИМЕРА 3
 
 
 
@@ -124,37 +72,37 @@ class LyricsDownloader extends SongLyrics {
 //import java.net.URL;
 //
 //abstract class SongLyrics {
-//    protected abstract String getLyrics(String artist, String song);
+//    protected abstract String getLyrics(String song);
 //}
 //
 //public class Task5_5_2 {
-//    public static void main(String[] args) {
+//    public static void main(String[] args) throws IOException {
 //        System.out.println("""
 //                Задание:\s
 //                Модуль 5. Интерфейсы, абстрактные классы, статические методы. Задание №5:\s
-//                    1. Создайте абстрактный класс SongLyrics, который возвращает текст песни
-//                       по названию.\s
+//                    2. Создайте файл в формате: Название песни: текст песни, и так много песен
+//                       (можете разделять разные песни пустыми строками, или другим способом)
+//                       Создайте наследника - FileSongLyrics, который ищет в файле по названию
+//                        песни, и если находит, возвращает текст песни.\s
 //
 //                Решение:\s""");
 //
-//        LyricsDownloader lyricsDownloader = new LyricsDownloader();
-//        String artist = "Eminem";
+//        FileSongLyrics lyricsDownloader = new FileSongLyrics();
 //        String song = "Lose yourself";
 //
-//        String lyrics = lyricsDownloader.getLyrics(artist, song);
-//        System.out.println("Текст песни '" + song + "', исполнителя '" + artist + "':\n" + lyrics);
+//        String lyrics = lyricsDownloader.getLyrics(song);
+//        System.out.println("Выводим текст песни '" + song + "', исполнителя 'Eminem':\n" + lyrics);
 //    }
 //}
 //
-//class LyricsDownloader extends SongLyrics {
+//class FileSongLyrics extends SongLyrics {
 //
 //    @Override
-//    protected String getLyrics(String artist, String song) {
-//        String url = "https://api.lyrics.ovh/v1/" + artist + "/" + song.replace(" ", "%20");
+//    protected String getLyrics(String song) {
 //        StringBuilder response = new StringBuilder();
 //
 //        try {
-//            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+//            HttpURLConnection connection = (HttpURLConnection) new URL(getURL("Eminem", song)).openConnection();
 //            connection.setRequestMethod("GET");
 //
 //            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
@@ -167,9 +115,12 @@ class LyricsDownloader extends SongLyrics {
 //            e.printStackTrace();
 //        }
 //
-//        // Обработка JSON ответа, чтобы получить текст песни
 //        String lyrics = response.toString().split("\"lyrics\":\"")[1].split("\"")[0];
 //        return lyrics;
+//    }
+//
+//    private String getURL(String artist, String song) {
+//        return "https://api.lyrics.ovh/v1/" + artist + "/" + song.replace(" ", "%20");
 //    }
 //}
 //// КОНЕЦ ПРИМЕРА 2
