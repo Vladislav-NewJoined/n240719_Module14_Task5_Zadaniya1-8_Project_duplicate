fb22abb8b1e21a1ee41df2473248039607a73190
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-04-03 14:42:05 +0300

    Initial Commit

---
+++ src/task6_1_1/Task6_1_1.java
@@ -0,0 +1,15 @@
+package task6_1_1;
+
+public class Task6_1_1 {
+    public static void main(String[] args) {
+        System.out.println("""
+                Задание:\s
+                Модуль 6. Основные структуры данных. Задание №1:\s
+                    1. Что такое компаратор?\s
+
+                Решение:\s""");
+
+        System.out.println("""
+                Some text.\s""");
+    }
+}
