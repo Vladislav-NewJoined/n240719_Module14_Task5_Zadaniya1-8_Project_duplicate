38bcbcbc5878b5373f2645235256a61e28f8cd10
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-04-03 11:32:30 +0300

    Initial Commit

---
+++ src/task5_6_0_VideoUrok/Task5_6_0_VideoUrok.java
@@ -0,0 +1,13 @@
+package task5_6_0_VideoUrok;
+
+public class Task5_6_0_VideoUrok {
+    public static void main(String[] args) {
+        System.out.println("""
+                Задание:\s
+                Модуль 5. Интерфейсы, абстрактные классы, статические методы. Задание №6:\s
+                    1. куда можно добавить модификатор доступа?\s
+
+                Решение:\s""");
+
+    }
+}
