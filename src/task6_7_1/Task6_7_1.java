959dfeed33f54b72aaf5400223d91312bf70e2f7
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-04-05 17:46:39 +0300

    Initial Commit

--- src/task6_7_1/Task6_7_1.java
+++ src/task6_7_1/Task6_7_1.java
@@ -24,8 +24,9 @@ public class Task6_7_1 {
         originalDatabase.put("David", 2);
         originalDatabase.put("Frank", 1);
         System.out.println("Исходная база данных студентов:");
-        System.out.println(originalDatabase);
-
+        for (String name : originalDatabase.keySet()) {
+            System.out.println(name + ": " + originalDatabase.get(name));
+        }
         // Создаем HashMap для хранения базы данных студентов
         HashMap<String, HashMap<Integer, Integer>> studentDatabase = new HashMap<>();
 
@@ -39,8 +40,16 @@ public class Task6_7_1 {
         // Бесконечный цикл для ввода данных пользователем
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.print("Введите имя студента и его оценку через запятую (например, 'Petya,5'): ");
+            System.out.println("""
+                    Введите имя студента и его оценку через запятую (например, 'Petya,5'),
+                    можно ввести нового студента, например 'Vasya,5',
+                    или введите 'end' для завершения программы:\s""");
             String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                System.out.println("Программа завершена.");
+                break;
+            }
 
             // Парсим введенную строку на имя и оценку
             String[] parts = input.split(",");
@@ -50,7 +59,13 @@ public class Task6_7_1 {
             }
 
             String studentName = parts[0].trim();
-            int grade = Integer.parseInt(parts[1].trim());
+            int grade;
+            try {
+                grade = Integer.parseInt(parts[1].trim());
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка ввода оценки. Попробуйте еще раз.");
+                continue;
+            }
 
             // Если студент уже есть в базе данных, добавляем новую оценку в список
             if (studentDatabase.containsKey(studentName)) {
@@ -80,6 +95,90 @@ public class Task6_7_1 {
     }
 }
 
+
+
+
+//// ПРИМЕР 4
+//import java.util.HashMap;
+//import java.util.Scanner;
+//
+//public class Task6_7_1 {
+//    public static void main(String[] args) {
+//        System.out.println("""
+//                Задание:\s
+//                Модуль 6. Основные структуры данных. Задание №7:\s
+//                    1. Вам необходимо используя HashMap написать базу данных студентов, реализовать
+//                       бесконечный цикл, пользователь вводит имя студента и оценку и в случае если
+//                       студент уже внесен в данную базу данных, вводимая оценка должна сохраняться в
+//                       списке и после каждого ввода необходимо осуществлять вывод на экран средней
+//                       оценки всех студентов.\s
+//
+//                Решение:\s""");
+//
+//        // Исходная база данных студентов
+//        HashMap<String, Integer> originalDatabase = new HashMap<>();
+//        originalDatabase.put("John", 4);
+//        originalDatabase.put("Petya", 3);
+//        originalDatabase.put("Michael", 5);
+//        originalDatabase.put("David", 2);
+//        originalDatabase.put("Frank", 1);
+//        System.out.println("Исходная база данных студентов:");
+//        System.out.println(originalDatabase);
+//
+//        // Создаем HashMap для хранения базы данных студентов
+//        HashMap<String, HashMap<Integer, Integer>> studentDatabase = new HashMap<>();
+//
+//        // Копируем исходную базу данных в studentDatabase
+//        for (String name : originalDatabase.keySet()) {
+//            HashMap<Integer, Integer> grades = new HashMap<>();
+//            grades.put(originalDatabase.get(name), 1);
+//            studentDatabase.put(name, grades);
+//        }
+//
+//        // Бесконечный цикл для ввода данных пользователем
+//        Scanner scanner = new Scanner(System.in);
+//        while (true) {
+//            System.out.print("Введите имя студента и его оценку через запятую (например, 'Petya,5'): ");
+//            String input = scanner.nextLine();
+//
+//            // Парсим введенную строку на имя и оценку
+//            String[] parts = input.split(",");
+//            if (parts.length != 2) {
+//                System.out.println("Ошибка ввода. Попробуйте еще раз.");
+//                continue;
+//            }
+//
+//            String studentName = parts[0].trim();
+//            int grade = Integer.parseInt(parts[1].trim());
+//
+//            // Если студент уже есть в базе данных, добавляем новую оценку в список
+//            if (studentDatabase.containsKey(studentName)) {
+//                HashMap<Integer, Integer> gradesMap = studentDatabase.get(studentName);
+//                gradesMap.put(grade, gradesMap.getOrDefault(grade, 0) + 1);
+//            } else {
+//                // Если студента нет в базе данных, создаем новую запись
+//                HashMap<Integer, Integer> gradesMap = new HashMap<>();
+//                gradesMap.put(grade, 1);
+//                studentDatabase.put(studentName, gradesMap);
+//            }
+//
+//            // Выводим список всех студентов и их средние оценки
+//            System.out.println("Список всех студентов и их средние оценки:");
+//            for (String name : studentDatabase.keySet()) {
+//                HashMap<Integer, Integer> gradesMap = studentDatabase.get(name);
+//                double totalGrade = 0.0;
+//                int totalStudents = 0;
+//                for (int g : gradesMap.keySet()) {
+//                    totalGrade += g * gradesMap.get(g);
+//                    totalStudents += gradesMap.get(g);
+//                }
+//                double averageGrade = totalGrade / totalStudents;
+//                System.out.println(name + ": " + averageGrade);
+//            }
+//        }
+//    }
+//}
+//// КОНЕЦ ПРИМЕРА 4
 
 
 
