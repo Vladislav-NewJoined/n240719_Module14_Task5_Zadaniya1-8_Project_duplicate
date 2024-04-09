c4d0d146b95e78bed25827a7f6be3d00fa787ce1
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-04-09 07:01:55 +0300

    Initial Commit

--- src/a_shablony_A_Exercises/Exercises.java
+++ src/a_shablony_A_Exercises/Exercises.java
@@ -10,8 +10,12 @@ public class Exercises {
         System.out.println(greeting); // Выводим результат на экран
 
         Helper2 helper2 = new Helper2(); // Создаем объект класса Helper2
-        helper2.method2("", ""); // Вызываем метод method2()
+        helper2.method2("", "PARAMETER2"); // Вызываем метод method2()
         System.out.println(helper2.getHello); // Выводим результат на экран
+
+        Helper3 helper3 = new Helper3(); // Создаем объект класса Helper2
+        helper3.parameter3("PARAMETER4", "PARAMETER5"); // Вызываем метод method2()
+        System.out.println(helper3.parameter3);
     }
 }
 
@@ -26,8 +30,20 @@ class Helper2 {
     String getHello;
 
     public void method2(String parameter1, String parameter2) {
-        getHello = "Привет из класса Helper2!"; // Инициализация переменной getHello в методе
+        getHello = "Привет из класса Helper2! Значение parameter2 = " + parameter2; // Инициализация переменной getHello в методе
 
+    }
+}
+
+class Helper3 {
+    String parameter3;
+
+    public String parameter3(String parameter4, String parameter5) {
+        parameter3 = "Привет из класса Helper3! Значение parameter3 = PARAMETER3" +
+                ", Значение parameter4 = " + parameter4 +
+                ", Значение parameter5 = " + parameter5; // Инициализация переменной parameter3 в методе
+
+        return parameter3; // Возвращаем значение parameter3
     }
 }
 
