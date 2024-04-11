09e854b658a043e9025c0825535f76614828d0c1
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-04-11 13:47:50 +0300

    Initial commit

--- src/task7_5_1/Task7_5_1.java
+++ src/task7_5_1/Task7_5_1.java
@@ -6,7 +6,7 @@ public class Task7_5_1 {
     public static void main(String[] args) {
         System.out.println("""
                 Задание:\s
-                Модуль 7. Взаимодействие с API. Задание №3:\s
+                Модуль 7. Взаимодействие с API. Задание №5:\s
                     1. У вас есть список моделей смартфонов, iPhone 6 S", "Lumia 950", "Samsung Galaxy S 6", "LG G 4",
                        "Nexus 7", вам нужно вывести в консоль названия двух моделей пропустив при этом первый бренд
                        при помощи  метода skip и limit.\s
