899b8c88c2825b72e91c14087bf215260a8a90a6
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-04-11 10:12:26 +0300

    Initial commit

--- src/task7_4_1/Task7_4_1.java
+++ src/task7_4_1/Task7_4_1.java
@@ -1,5 +1,9 @@
 package task7_4_1;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class Task7_4_1 {
     public static void main(String[] args) {
         System.out.println("""
@@ -10,6 +14,12 @@ public class Task7_4_1 {
                        и выведите их  в консоль.\s
 
                 Решение:\s""");
-
+        Random random = new Random();
+        System.out.println("Создаём рандомный поток из следующих чисел: ");
+        int[] array = {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(Arrays.toString(array));
+        System.out.println("Обрабатываем его с помощью Stream, согласно заданию: ");
+        IntStream intStream2 = IntStream.generate(()->random.nextInt( -10, 11)).limit(10);
+        intStream2.filter((a) -> a > 0).filter((a) -> a <= 10).sorted().forEach(System.out::println);
     }
 }
