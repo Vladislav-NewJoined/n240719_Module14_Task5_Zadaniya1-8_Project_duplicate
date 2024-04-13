bc7007de733b1878fdfd092b88da3c73ebae1dd4
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-04-13 12:56:50 +0300

    Initial commit

---
+++ src/task8_1_1/Task7_5_1.java
@@ -0,0 +1,13 @@
+package task8_1_1;
+
+public class Task7_5_1 {
+    public static void main(String[] args) {
+        System.out.println("""
+                Задание:\s
+                Модуль 8. Многопоточность. Задание №1:\s
+                    1. Создать класс NewThread расширяющий Thread.\s
+
+                Решение:\s""");
+
+    }
+}
