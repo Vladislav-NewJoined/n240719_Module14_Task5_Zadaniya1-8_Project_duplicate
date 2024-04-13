8acc43998a731b6bcca39a3b0f1e990c09c97625
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-04-13 18:57:02 +0300

    Initial commit

---
+++ src/task8_2_1/Task8_2_1.java
@@ -0,0 +1,14 @@
+package task8_2_1;
+
+public class Task8_2_1 {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("""
+                Задание:\s
+                Модуль 8. Многопоточность. Задание №2:\s
+                    1. Напишите программу, которая сможет в одном потоке читать данные из консоли, а в другом потоке
+                       будет их выводить.\s
+
+                Решение:\s""");
+
+    }
+}
