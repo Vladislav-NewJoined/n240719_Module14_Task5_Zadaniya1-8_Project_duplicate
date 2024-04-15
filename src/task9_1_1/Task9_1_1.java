b363f2f86ce3acc5184f940fdbcc507d24e5dc6c
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-04-15 08:56:10 +0300

    Initial commit

---
+++ src/task9_1_1/Task9_1_1.java
@@ -0,0 +1,13 @@
+package task9_1_1;
+
+public class Task9_1_1 {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("""
+                Задание:\s
+                Модуль 9. UI и использование готовых SDK. Задание №1:\s
+                    1. Опишите своими словами что такое Maven?
+
+                Решение:\s""");
+
+    }
+}
