5ec66194e30c8ab28c8408147a5dd2f2e61d4ddb
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-04-16 16:47:22 +0300

    Initial commit

---
+++ src/task9_2_1/Task9_2_1.java
@@ -0,0 +1,15 @@
+package task9_2_1;
+
+public class Task9_2_1 {
+    public static void main(String[] args) {
+        System.out.println("""
+                Задание:\s
+                Модуль 9. UI и использование готовых SDK. Задание №2:\s
+                    1. В данном модуле вам предстоит разработать телеграм бота, а именно  в данном задании нужно 
+                       создать Maven проект и добавить зависимость  api telegram bots и telegram bots meta\s
+
+                Решение:\s""");
+
+        System.out.println("**********************");
+    }
+}
