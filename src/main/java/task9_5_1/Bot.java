3242fcda9b954195f2614a49c0738ab16b56a076
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-05-02 16:45:44 +0300

    Initial commit

--- src/main/java/task9_5_1/Bot.java
+++ src/main/java/task9_5_1/Bot.java
@@ -8,6 +8,9 @@ import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import task9_5_1.functions.FilterOperation;
 import task9_5_1.utils.ImageUtils;
@@ -16,6 +19,7 @@ import task9_5_1.utils.RgbMaster;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class Bot extends TelegramLongPollingBot {
     @Override
@@ -93,6 +97,24 @@ public class Bot extends TelegramLongPollingBot {
         }
         inputStream.close();
         outputStream.close();
+    }
+
+    private SendPhoto preparePhotoMessage(String localPath, String chatId) {
+        SendPhoto sendPhoto = new SendPhoto();
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+//        keyboardRows.add(new KeyboardRow());
+        for (int i = 0; i < 3; i++) {
+            KeyboardButton keyboardButton = new KeyboardButton("button" + i);
+            keyboardRows.add(new KeyboardRow());
+        }
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+        sendPhoto.setReplyMarkup();
+        sendPhoto.setChatId(chatId);
+        InputFile newFile = new InputFile();
+        newFile.setMedia(new File(localPath));
+        sendPhoto.setPhoto(newFile);
+        return sendPhoto;
     }
 }
 //// КОНЕЦ ПРИМЕРА
