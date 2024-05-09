5e23b7f3f880c78ef00537948a54ba6f2a7174e9
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-05-09 14:59:41 +0300

    Initial commit

---
+++ src/main/java/task9_7_1/Task9_7_1.java
@@ -0,0 +1,22 @@
+package task9_7_1;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.generics.BotSession;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import task9_5_1.Bot;
+
+public class Task9_7_1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("""
+            Задание:\s
+            Модуль 9. UI и использование готовых SDK. Задание №7:\s
+                1. Продолжая работу над ботом из предыдущих уроков, реализуйте возможность вызова соответствующего
+                   обработчика (класса)  в зависимости от того, прислал пользователь изображение или текстовое
+                   сообщение\s
+
+            Решение:\s""");
+
+        System.out.println("В Telegram боте созданы .");
+
+    }
+}
