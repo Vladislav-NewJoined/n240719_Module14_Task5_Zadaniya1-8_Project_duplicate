0c5c26c7acbc11eabf50faa0149a09e2c281df0f
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-05-13 10:57:18 +0300

    Initial commit

--- src/main/java/task9_5_1/Bot.java
+++ src/main/java/task9_5_1/Bot.java
@@ -24,12 +24,12 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return "my_gfjhfghfjhgfjhgfghfhgfjf_bot"; // Название вашего бота
+        return "qytewqwww_Bot"; // Название вашего бота
     }
 
     @Override
     public String getBotToken() {
-        return "6750924950:AAGOE5XBnuJDlNIKHYq61S7bMupKXhKRZZo"; // Токен вашего бота
+        return "7057416920:AAEzJF-2L8i8GdyLnkqMThUyyXk6BQOdoAk"; // Токен вашего бота
     }
 
     @Override
