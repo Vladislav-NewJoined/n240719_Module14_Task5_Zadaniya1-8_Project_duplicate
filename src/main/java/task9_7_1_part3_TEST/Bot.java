ad6e5910b72ee5b6a1a4bc57d5dc405822c25d07
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-05-26 17:41:13 +0300

    Initial commit

--- src/main/java/task9_7_1_part3_TEST/Bot.java
+++ src/main/java/task9_7_1_part3_TEST/Bot.java
@@ -1,6 +1,6 @@
 package task9_7_1_part3_TEST;
 
-// ПОДХОД к ПРИМЕРу 8 240526 ____  достиг ПРИМЕРа 8 т.е. до конца ВИДЕО 08 _Пытаюсь объединить preparePhotoMessage
+// ПОДХОД к ПРИМЕРу 8 240526 ____ Прододжаю пытаться объединить preparePhotoMessage
 // Дорабатываем Пример 3 _РАБОТАЕТ! Создаёт 3 кнопки и обрабатывает их, и создаёт лишнюю надпись "Команда не из кнопки".
 // И обрабатывает только одно изображение _Использован бот №2
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -279,6 +279,288 @@ public class Bot extends TelegramLongPollingBot {
     }
 }
 // КОНЕЦ ПОДХОДа к ПРИМЕРу 8 240526 ____
+
+
+
+// ПОДХОД к ПРИМЕРу 8 240526 1707 _Пытаюсь объединить preparePhotoMessage
+// Дорабатываем Пример 3 _РАБОТАЕТ! Создаёт 3 кнопки и обрабатывает их, и создаёт лишнюю надпись "Команда не из кнопки".
+// И обрабатывает только одно изображение _Использован бот №2
+//import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+//import org.telegram.telegrambots.meta.api.methods.GetFile;
+//import org.telegram.telegrambots.meta.api.methods.send.SendMediaGroup;
+//import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+//import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+//import org.telegram.telegrambots.meta.api.objects.InputFile;
+//import org.telegram.telegrambots.meta.api.objects.Message;
+//import org.telegram.telegrambots.meta.api.objects.PhotoSize;
+//import org.telegram.telegrambots.meta.api.objects.Update;
+//import org.telegram.telegrambots.meta.api.objects.media.InputMedia;
+//import org.telegram.telegrambots.meta.api.objects.media.InputMediaPhoto;
+//import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+//import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+//import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+//import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+//import task9_7_1_part3_TEST.commands.BotCommonCommands;
+//import task9_7_1_part3_TEST.commands.AppBotCommand;
+//import task9_7_1_part3_TEST.functions.FilterOperation;
+//import task9_7_1_part3_TEST.functions.ImageOperation;
+//import task9_7_1_part3_TEST.utils.PhotoMessageUtils;
+//
+//import java.io.*;
+//import java.lang.reflect.InvocationTargetException;
+//import java.lang.reflect.Method;
+//import java.net.URL;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static task9_7_1_part3_TEST.utils.PhotoMessageUtils.processingImage;
+//
+//public class Bot extends TelegramLongPollingBot {
+//
+//    Class[] commandClasses = new Class[] {BotCommonCommands.class};
+//
+//    @Override
+//    public String getBotUsername() {
+//        return "lgcyrrerrr_002_bot"; // Название вашего бота
+//    }
+//
+//    @Override
+//    public String getBotToken() {
+//        return "7042048331:AAFXdhcICubXad5RxZGyItCIi7L6IUwvaJo"; // Токен вашего бота
+//    }
+//
+//    @Override
+//    public void onUpdateReceived(Update update) {
+//        Message message2 = update.getMessage();
+//        try {
+//            SendMessage responseTextMessage2 = runCommonCommand(message2);
+//            if (responseTextMessage2 != null) {
+//                execute(responseTextMessage2);     // TODO ОСТАВЛЯЕМ! ответ на кнопки help hello bye, вызываем библиотечный класс SendMessage
+//                return;
+//            }
+//        } catch (InvocationTargetException | IllegalAccessException | TelegramApiException e) {
+//            e.printStackTrace();
+//        }
+//
+//        Message message = update.getMessage();
+//        String chatId = message.getChatId().toString();
+//
+//        String response = null;
+//        response = runCommand(message.getText());     // TODO ОСТАВЛЯЕМ! возврат изображениий (1-го и 4-х), вызывает метод runCommand
+//        SendMessage sendMessage = new SendMessage();
+//        sendMessage.setChatId(chatId);
+//        sendMessage.setText(response);
+//        final String localFileName = "src/main/java/task9_7_1_part3_TEST/" + "cloned_image.jpg";
+//        PhotoSize photoSize = message.getPhoto().get(0);
+//        response = runCommand(message.getText());
+//        final String fileId = photoSize.getFileId();
+//        try {
+//            final org.telegram.telegrambots.meta.api.objects.File file = execute(new GetFile(fileId));
+//            final String imageUrl = "https://api.telegram.org/file/bot" + getBotToken() + "/" + file.getFilePath();
+//            saveImage(imageUrl, localFileName);     // TODO ОСТАВЛЯЕМ! ответ на кнопки и возврат изображений и сохранение их, вызов метода saveImage из класса preparePhotoMessage2
+//        } catch (TelegramApiException | IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        InputFile newFile = new InputFile();
+//        newFile.setMedia(new File(localFileName));
+//
+//        sendMessage.setChatId(chatId);
+//        sendMessage.setText(response);
+//
+//        try {
+//            execute(sendMessage);     // TODO ВНИМАНИЕ: ОСТАВЛЯЕМ, но с оговоркой!  отменило надпись: "Команда не из кнопки" НАДО ПЕРЕНЕСТИ на возврат неск. изображений
+//        } catch (TelegramApiException e) {
+//            e.printStackTrace();
+//        }
+//
+//        SendPhoto sendPhoto = preparePhotoMessage2(localFileName, message.getChatId().toString());
+//        ///
+//        sendPhoto.setPhoto(newFile);      // TODO ОСТАВЛЯЕМ!
+//        sendPhoto.setCaption("cloned_image");      // TODO ПЕРЕНОСИМ ВО ФРАГМЕНТ С ВОЗВРАТОМ НЕСКОЛЬКИХ ИЗОБРАЖЕНИЙ
+//
+//        try {
+//            execute(sendPhoto);     // TODO ВАЖНО: ХОРОШО! УДАЛЯЕМ применит. к неск. изображениям! ЭТО ОБРАЩЕНИЕ К КЛАССУ preparePhotoMessage2 С ДВУМЯ АРГУМЕНТАМИ! Без него не возвращает одно изображение, на кнопки отвечает, кнопки правда НЕ СОЗДАЁТ, возвращает неск. изображений (но без надписи "cloned image), с ним не отвечает на кнопки! без него сохранило, но не вернуло одно ЦВЕТНОЕ изображение, вызов метода preparePhotoMessage с двумя параметрами
+//        } catch (TelegramApiException e) {
+//            e.printStackTrace();
+//        }
+//        // TODO После этого:
+//        // TODO Возвращает надпись "Команда не из кнопки"
+//        // TODO Создаёт 4 кнопки, отвечает на них
+//        // TODO Возвращает 4 изображения, сохраняет их
+//        // TODO Возвращает надпись "cloned_image", только не в том месте
+//        // TODO ОТРИЦАТЕЛЬНЫЙ: Возвращает одно ЦВЕТНОЕ изображение
+//
+//
+//
+//
+//// Пишу для 7-го Примера
+//        try {
+//            SendMediaGroup responseMediaMessage2 = runPhotoFilter(message2);
+//            if (responseMediaMessage2 != null) {
+//                execute(responseMediaMessage2);     // TODO ОСТАВЛЯЕМ! т.к. без него не возвращает и не сохраняет неск. изображений. Кнопки создаёт, отвечает, НО возвращает ЦВЕТНОЕ изобр.,
+//                return;
+//            }
+//        } catch (/*InvocationTargetException | IllegalAccessException | */TelegramApiException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private String runCommand(String text)/* throws InvocationTargetException, IllegalAccessException*/ {
+//        BotCommonCommands commands = new BotCommonCommands();
+//        Method[] classMethods = commands.getClass().getDeclaredMethods();
+//
+//        for (Method method : classMethods) {
+//            if (method.isAnnotationPresent(AppBotCommand.class)) {
+//                AppBotCommand annotation = method.getAnnotation(AppBotCommand.class);
+//                if (annotation.name().equals(text)) {
+//                    try {
+//                        method.setAccessible(true);     // TODO ОСТАВЛЯЕМ! Т.к. без него в консоли появляются красные строки, но на телеграм бот не влияет
+//                        return (String) method.invoke(commands);
+//                    } catch (IllegalAccessException | InvocationTargetException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        }
+//        return "Команда не из кнопки";
+//    }
+//
+//
+//    // TODO Этого метода не было в ИЗНАЧАЛЬНЫХ МЕТОДАХ, НАПИСАН ПОЛНОСТЬЮ ЗАНОВО
+//    private SendMessage runCommonCommand(Message message2) throws InvocationTargetException, IllegalAccessException {
+//        String text2 = message2.getText();
+//        BotCommonCommands commands2 = new BotCommonCommands();
+//        Method[] classMethods2 = commands2.getClass().getDeclaredMethods();
+//        for (Method method : classMethods2) {
+//            if (method.isAnnotationPresent(AppBotCommand.class)) {
+//                AppBotCommand command = method.getAnnotation(AppBotCommand.class);
+//                if (command.name().equals(text2)) {
+////                    method.setAccessible(true);       //  TODO УДАЛЯЕМ! Не влияет.
+//                    String responseText2 = (String) method.invoke(commands2);       //  TODO ОСТАВЛЯЕМ! Без него на кнопки не отвечает
+//                    if (responseText2 != null) {
+//                        SendMessage sendMessage2 = new SendMessage();
+//                        sendMessage2.setChatId(message2.getChatId().toString());       //  TODO ОСТАВЛЯЕМ! Без него на кнопки не отвечает
+//                        sendMessage2.setText(responseText2);       //  TODO ОСТАВЛЯЕМ! Без него на кнопки не отвечает
+//                        return sendMessage2;
+//                    }
+//                }
+//            }
+//        }
+//        return null;
+//    }
+//
+//    // TODO Этого метода не было в ИЗНАЧАЛЬНЫХ МЕТОДАХ, НАПИСАН ПОЛНОСТЬЮ ЗАНОВО
+//    private SendMediaGroup runPhotoFilter (Message message2) {
+//        ImageOperation operation2 = FilterOperation::greyScale;
+//        List<org.telegram.telegrambots.meta.api.objects.File> files2 = getFilesByMessage(message2);
+//        try {
+//            List<String> paths2 = PhotoMessageUtils.savePhotos(files2, getBotToken());       //  TODO ОСТАВЛЯЕМ! Без него 4 изображения не возвращает
+//            String chatId = message2.getChatId().toString();       //  TODO ОСТАВЛЯЕМ! Без него 4 изображения не возвращает
+//            return preparePhotoMessage2(paths2, operation2, chatId);       //  TODO ОСТАВЛЯЕМ! Без него 4 изображения не возвращает
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//    // TODO Этого метода не было в ИЗНАЧАЛЬНЫХ МЕТОДАХ, НАПИСАН ПОЛНОСТЬЮ ЗАНОВО
+//    private List<org.telegram.telegrambots.meta.api.objects.File> getFilesByMessage(Message message2) { // TODO Это новое, но пока не стал менять на просто File (или java.io.File)/ в видеоуроке 09 52 мин
+////    private List<org.telegram.telegrambots.meta.api.objects.File> getFilesByMessage(Message message) { // TODO Так было изначально
+//        List<PhotoSize> photoSizes = message2.getPhoto();
+//        ArrayList<org.telegram.telegrambots.meta.api.objects.File> files2 = new ArrayList<>(); // TODO Это новое, но пока не стал менять на просто File (или java.io.File)/ в видеоуроке 09 52 мин
+////        ArrayList<org.telegram.telegrambots.meta.api.objects.File> files2 = new ArrayList<>(); // TODO Так было изначально
+//        for (PhotoSize photoSize : photoSizes){
+//            final String fileId = photoSize.getFileId();
+//            try {
+//                files2.add(sendApiMethod(new GetFile(fileId)));       //  TODO ОСТАВЛЯЕМ! Без него 4 изображения не возвращает
+//            } catch (TelegramApiException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        return files2;
+//    }
+//
+//    private SendMediaGroup preparePhotoMessage2(List<String> localPaths2, ImageOperation operation2, String chatId) throws Exception {
+//        SendMediaGroup mediaGroup2 = new SendMediaGroup();
+//        ArrayList<InputMedia> medias2 = new ArrayList<>();
+//        for (String path2 : localPaths2) {
+//            InputMedia inputMedia2 = new InputMediaPhoto();
+//            PhotoMessageUtils.processingImage2(path2, operation2);
+//            inputMedia2.setMedia(new File(path2), "path2");
+//            medias2.add(inputMedia2);
+//        }
+//        mediaGroup2.setMedias(medias2);
+//        mediaGroup2.setChatId(chatId);
+//
+//        // Отправка группы обработанных изображений
+//        execute(mediaGroup2);
+//
+//        // Отправка текстового сообщения "cloned_image"
+//        SendMessage sendMessage = new SendMessage();
+//        sendMessage.setChatId(chatId);
+//        sendMessage.setText("cloned_image");
+//        execute(sendMessage);
+//
+//        return mediaGroup2;
+//    }
+//
+//    private void saveImage(String url, String fileName) throws IOException {
+//        URL urlModel = new URL(url);
+//        InputStream inputStream = urlModel.openStream();
+//        OutputStream outputStream = new FileOutputStream(fileName);
+//        byte[] b = new byte[2048];
+//        int length;
+//        while ((length = inputStream.read(b)) != -1) {
+//            outputStream.write(b, 0, length);         //  TODO ВНИМАНИЕ: ВСЁ ТАКИ ООСТАВЛЯЕМ, т.к. без него кнопки не создаёт!  УДАЛЯЕМ или? И без него кнопки не создаёт! ЗДЕСЬ ПРАВИЛЬНО: одно изобр. не возвращает, группу - возвращает. Но не возвращает надпись: "cloned_image"
+//        }
+//        inputStream.close();
+//        outputStream.close();
+//    }
+//
+//
+//    private SendPhoto preparePhotoMessage2(String localPath, String chatId) {
+//        SendPhoto sendPhoto = new SendPhoto();
+//// TODO НАШЁЛ! ЭТА СТРОКА ОТВЕЧАЕТ ЗА СОЗДАНИЕ ТРЁХ КНОПОК в ПРИМЕРЕ СНАЧАЛА 4, ТЕПЕРЬ 6, в методе SendPhoto preparePhotoMessage2
+//        sendPhoto.setReplyMarkup(getKeyboard()); // Это ТРИ кнопки  // TODO в ПРИМЕРЕ СНАЧАЛА 4, ТЕПЕРЬ 6 В методе preparePhotoMessage2. ДА! При комменте три кнопки не создадутся!
+//        sendPhoto.setChatId(chatId);
+//        InputFile newFile = new InputFile();
+//        newFile.setMedia(new File(localPath)); // TODO Здесь заменил File на java.io.File как в видеоуроке на мин 10 22
+////        newFile.setMedia(new File(localPath)); // TODO Так было изначально
+//        sendPhoto.setPhoto(newFile);
+//        return sendPhoto;
+//    }
+//
+//    private ReplyKeyboardMarkup getKeyboard() {
+//        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+//        ArrayList<KeyboardRow> allKeyboardRows = new ArrayList<>();
+//        allKeyboardRows.addAll(getKeyboardRows(BotCommonCommands.class));
+//        allKeyboardRows.addAll(getKeyboardRows(FilterOperation.class));
+//
+//        replyKeyboardMarkup.setKeyboard(allKeyboardRows);
+//        replyKeyboardMarkup.setOneTimeKeyboard(true);
+//        return replyKeyboardMarkup;
+//    }
+//
+//    private ArrayList<KeyboardRow> getKeyboardRows(Class someClass) {
+//        Method[] classMethods = someClass.getDeclaredMethods();
+//        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+//        KeyboardRow row = new KeyboardRow();
+//
+//        for (Method method : classMethods) {
+//            if (method.isAnnotationPresent(AppBotCommand.class)) {
+//                AppBotCommand annotation = method.getAnnotation(AppBotCommand.class);
+//                KeyboardButton button = new KeyboardButton();
+//                button.setText(annotation.name());
+//                row.add(button);
+//            }
+//        }
+//
+//        keyboardRows.add(row);
+//        return keyboardRows;
+//    }
+//}
+// КОНЕЦ ПОДХОДа к ПРИМЕРу 8 240526 1707
 
 
 
