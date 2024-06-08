1238751ef4f3a8a7028c4b31d8d30b5c82e4f47b
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-06-08 17:04:46 +0300

    Initial commit

---
+++ src/main/java/task9_7_1_part7/utils/ImageUtils.java
@@ -0,0 +1,38 @@
+package task9_7_1_part7.utils;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageUtils {
+    static public BufferedImage getImage(String path) throws IOException {
+        final File file = new File(path);
+        ImageIO.read(file);
+        return ImageIO.read(file);
+    }
+
+    static public void saveImage(BufferedImage image, String path) throws IOException {
+        ImageIO.write(image, "jpg", new File(path));
+    }
+
+    static float[] rgbIntToArray(int pixel) {
+        Color color = new Color(pixel);
+        return color.getRGBComponents(null);
+
+    }
+
+    static int arrayToRgbInt(float[] pixel) throws Exception {
+        Color color = null;
+        if (pixel.length == 3) {
+            color = new Color(pixel[0], pixel[1], pixel[2]);
+        } else if (pixel.length == 4) {
+            color = new Color(pixel[0], pixel[1], pixel[2], pixel[3]);
+        }
+        if (color != null) {
+            return color.getRGB();
+        }
+        throw new Exception("invalid color");
+    }
+}
