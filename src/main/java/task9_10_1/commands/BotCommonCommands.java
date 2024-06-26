f7caa1d08f118e98761c6fd1cd95879b3fbdb49f
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-06-26 11:09:21 +0300

    240626 Initial commit

---
+++ src/main/java/task9_10_1/commands/BotCommonCommands.java
@@ -0,0 +1,20 @@
+package task9_10_1.commands;
+
+public class BotCommonCommands {
+    @AppBotCommand(name = "/hello", description = "when request hello", showInHelp = true)
+//    @task9_10_1.commands.AppBotCommand(name = "/hello", description = "when request hello", showInHelp = true)
+    public String hello() {
+        return "Hello, User";
+    }
+
+    @AppBotCommand(name = "/bye", description = "when request bye", showInHelp = true)
+//    @task9_10_1.commands.AppBotCommand(name = "/bye", description = "when request bye", showInHelp = true)
+    public String bye() {
+        return "Good bye, User";
+    }
+
+    @AppBotCommand(name = "/help", description = "when request help", showInKeyboard = true)
+    public String help() {
+        return "HEEEELP";
+    }
+}
--- src/main/java/task9_9_1/functions/ImageOperation.java
+++
@@ -1,7 +0,0 @@
-package task9_9_1.functions;
-
-import java.lang.reflect.InvocationTargetException;
-
-public interface ImageOperation {
-    float[] execute(float[] rgb) throws InvocationTargetException, IllegalAccessException;
-}
