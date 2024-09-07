1e2895da4dcbb8df733420b6b239a2576db5dce2
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-09-07 19:16:05 +0300

    240907 commit

--- src/main/java/task14_5_1/zadanye8/QuotesApplication8.java
+++ src/main/java/task14_5_1/zadanye8/QuotesApplication8.java
@@ -1,5 +1,6 @@
 package task14_5_1.zadanye8;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -10,52 +11,128 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.User;
 
 @SpringBootApplication
-public class QuotesApplication8 extends TelegramLongPollingBot {
+public class QuotesApplication8 implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(QuotesApplication8.class, args);
-
-        QuotesApplication8 bot = new QuotesApplication8();
-        SendMessage message = new SendMessage();
-        message.setText("Bot started");
-        message.setChatId("5799431854"); // Укажите chatId для отправки сообщения о запуске бота
-        try {
-            bot.execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
-    public void onUpdateReceived(Update update) {
-        Message message = update.getMessage();
-        User user = message.getFrom();
-
-        if (message.hasText()) {
-            String text = message.getText();
-
-            if ("/hello".equals(text)) {
-                SendMessage response = new SendMessage();
-                response.setChatId(message.getChatId().toString());
-                response.setText("Hello, " + user.getFirstName() + "!");
-
-                try {
-                    execute(response);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+    public void run(String... args) throws Exception {
     }
-
-    @Override
-    public String getBotUsername() {
-        return "kkkllll_005_bot"; // замените на username вашего бота
-    }
-
-    @Override
-    public String getBotToken() {
-        return "6882256834:AAH5Fg-wUdKw7Rdqj8s9kXDgVt0R08tDnlY"; // замените на token вашего бота
-    }
-
+//
+//        QuotesApplication8 bot = new QuotesApplication8();
+//        SendMessage message = new SendMessage();
+//        message.setText("Bot started");
+//        message.setChatId("5799431854"); // Укажите chatId для отправки сообщения о запуске бота
+//        try {
+//            bot.execute(message);
+//        } catch (TelegramApiException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Override
+//    public void onUpdateReceived(Update update) {
+//        Message message = update.getMessage();
+//        User user = message.getFrom();
+//
+//        if (message.hasText()) {
+//            String text = message.getText();
+//
+//            if ("/hello".equals(text)) {
+//                SendMessage response = new SendMessage();
+//                response.setChatId(message.getChatId().toString());
+//                response.setText("Hello, " + user.getFirstName() + "!");
+//
+//                try {
+//                    execute(response);
+//                } catch (TelegramApiException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public String getBotUsername() {
+//        return "kkkllll_005_bot"; // замените на username вашего бота
+//    }
+//
+//    @Override
+//    public String getBotToken() {
+//        return "6882256834:AAH5Fg-wUdKw7Rdqj8s9kXDgVt0R08tDnlY"; // замените на token вашего бота
+//    }
+//
 }
+
+
+
+
+
+
+
+
+
+
+//package task14_5_1.zadanye8;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+//import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+//import org.telegram.telegrambots.meta.api.objects.Update;
+//import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+//import org.telegram.telegrambots.meta.api.objects.Message;
+//import org.telegram.telegrambots.meta.api.objects.User;
+//
+//@SpringBootApplication
+//public class QuotesApplication8 extends TelegramLongPollingBot {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(QuotesApplication8.class, args);
+//
+//        QuotesApplication8 bot = new QuotesApplication8();
+//        SendMessage message = new SendMessage();
+//        message.setText("Bot started");
+//        message.setChatId("5799431854"); // Укажите chatId для отправки сообщения о запуске бота
+//        try {
+//            bot.execute(message);
+//        } catch (TelegramApiException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Override
+//    public void onUpdateReceived(Update update) {
+//        Message message = update.getMessage();
+//        User user = message.getFrom();
+//
+//        if (message.hasText()) {
+//            String text = message.getText();
+//
+//            if ("/hello".equals(text)) {
+//                SendMessage response = new SendMessage();
+//                response.setChatId(message.getChatId().toString());
+//                response.setText("Hello, " + user.getFirstName() + "!");
+//
+//                try {
+//                    execute(response);
+//                } catch (TelegramApiException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public String getBotUsername() {
+//        return "kkkllll_005_bot"; // замените на username вашего бота
+//    }
+//
+//    @Override
+//    public String getBotToken() {
+//        return "6882256834:AAH5Fg-wUdKw7Rdqj8s9kXDgVt0R08tDnlY"; // замените на token вашего бота
+//    }
+//
+//}
---
+++ src/main/java/task14_5_1/zadanye8/controllers/BotController.java
@@ -0,0 +1,137 @@
+package task14_5_1.zadanye8.controllers;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import task14_5_1.zadanye8.models.Chat;
+import task14_5_1.zadanye8.models.Quote;
+import task14_5_1.zadanye8.repositories.ChatRepository;
+import task14_5_1.zadanye8.services.BashParser;
+import task14_5_1.zadanye8.services.QuoteService;
+
+import java.util.Map;
+
+@Service
+public class BotController {
+
+    @Autowired
+    ChatRepository chatRepository;
+
+    @Autowired
+    QuoteService service;
+
+    private final TelegramBot bot;
+
+    public BotController(){
+// Create your bot passing the token received from @BotFather
+        String botToken = "6882256834:AAH5Fg-wUdKw7Rdqj8s9kXDgVt0R08tDnlY";
+        bot = new TelegramBot(botToken);
+
+// Register for updates
+        bot.setUpdatesListener(updates -> {
+
+            for (Update update : updates) {
+                handleUpdate(update);
+            }
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+
+
+
+
+
+//    @Autowired
+//    private BashParser bashParser;
+//
+//    @Autowired
+////    private TelegramBot bot;
+
+
+
+
+
+// Create Exception Handler
+//        }, e -> {
+//            if (e.response() != null) {
+//                // got bad response from telegram
+//                e.response().errorCode();
+//                e.response().description();
+//            } else {
+//                // probably network error
+//                e.printStackTrace();
+//            }
+        });
+    }
+
+    private void handleUpdate(Update update) {
+        String text = update.message().text();
+        long chatId = update.message().chat().id();
+        var rawChat = chatRepository.findByChatIdEquals(chatId);
+        Chat chat;
+        if (rawChat.isEmpty()) {
+            chat = rawChat.get();
+        } else {
+            var _chat = new Chat();
+            _chat.setChatId(chatId);
+            _chat.setLastId(0);
+            chat = chatRepository.save(_chat);
+        }
+        switch(text){
+            case "/start":
+            case "/next":
+                sendNextQuote(chat);
+                break;
+            case "/prev":
+                sendPrevQuote(chat);
+                break;
+            case "/rand":
+                sendRandom(chat);
+                break;
+        }
+
+//        if (text.equals("/getquote")) {
+//            Map.Entry<Integer, String> randomQuote = bashParser.getRandom();
+//            sendText(chatId, randomQuote.getValue());
+//        }
+
+    }
+    private void sendNextQuote(Chat chat) {
+        Quote quote = null;
+        int newId = chat.getLastId();
+        while (quote == null) {
+            newId++;
+            quote = service.getById(newId);
+
+        }
+        chat.setLastId(quote.getQuoteid());
+        chatRepository.save(chat);
+        sendText(chat.getChatId(), quote.getText());
+    }
+
+    private void sendPrevQuote(Chat chat) {
+        Quote quote = null;
+        int newId = chat.getLastId();
+        while (quote == null) {
+            newId--;
+            if (newId < 2) newId = 2;
+            quote = service.getById(newId);
+
+        }
+        chat.setLastId(quote.getQuoteid());
+        chatRepository.save(chat);
+        sendText(chat.getChatId(), quote.getText());
+    }
+
+    private void sendRandom(Chat chat) {
+        Quote quote = service.getRandom();
+        chatRepository.save(chat);
+        sendText(chat.getChatId(), quote.getText());
+    }
+
+    private void sendText(long chatId, String text) {
+        bot.execute(new SendMessage(chatId, text));
+    }
+
+}
