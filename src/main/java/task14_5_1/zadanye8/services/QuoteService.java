f07282cbf0f202165ffccf1c81fa9deb12e95099
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-09-07 20:00:12 +0300

    240907 commit

--- src/main/java/task14_5_1/zadanye8/services/QuoteService.java
+++ src/main/java/task14_5_1/zadanye8/services/QuoteService.java
@@ -26,17 +26,17 @@ public class QuoteService {
 
     public List<Quote> getPage(int page) {
         List<Quote> ret = new ArrayList<>();
-            Map<Integer, String> map = parser.getPage(page);
-            for (var entry : map.entrySet()) {
-                var rawQuote = new Quote();
-                rawQuote.setQuoteid(entry.getKey());
-                rawQuote.setText(entry.getValue());
-                var existed = repository.findByQuoteidEquals(rawQuote.getQuoteid());
-                if(existed.isEmpty())
-                    ret.add(repository.save(rawQuote));
-                else
-                    ret.add(existed.get());
-            }
+        Map<Integer, String> map = parser.getPage(page);
+        for (var entry : map.entrySet()) {
+            var rawQuote = new Quote();
+            rawQuote.setQuoteid(entry.getKey());
+            rawQuote.setText(entry.getValue());
+            var existed = repository.findByQuoteidEquals(rawQuote.getQuoteid());
+            if (existed.isEmpty())
+                ret.add(repository.save(rawQuote));
+            else
+                ret.add(existed.get());
+        }
         return ret;
     }
 
@@ -45,7 +45,7 @@ public class QuoteService {
         if (existingQuote.isPresent())
             return existingQuote.get();
         var quoteEntry = parser.getById(id);
-        if(quoteEntry == null)return null;
+        if (quoteEntry == null) return null;
         var newQuote = new Quote();
         newQuote.setQuoteid(quoteEntry.getKey());
         newQuote.setText(quoteEntry.getValue());
@@ -65,34 +65,35 @@ public class QuoteService {
         newQuote.setText(quoteEntry.getValue());
         return repository.save(newQuote);
     }
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    @Transactional
-    public void initDb() {
-        try {
-            String createQuotesTableQuery = "DROP TABLE IF EXISTS Quotes2;" +
-                    "CREATE TABLE IF NOT EXISTS Quotes2 (" +
-                    "id SERIAL PRIMARY KEY," +
-                    "text TEXT NOT NULL," +
-                    "quoteid INT NOT NULL" +
-                    ");";
-
-            String createChatsTableQuery = "DROP TABLE IF EXISTS Chats2;" +
-                    "CREATE TABLE IF NOT EXISTS Chats2 (" +
-                    "id BIGSERIAL PRIMARY KEY," +
-                    "chatId BIGINT NOT NULL," +
-                    "lastId INT NOT NULL" +
-                    ");";
-
-            entityManager.createNativeQuery(createQuotesTableQuery).executeUpdate();
-            entityManager.createNativeQuery(createChatsTableQuery).executeUpdate();
-            // Добавление других таблиц
-
-            System.out.println("Tables 'Quotes2' and 'Chats2' created successfully.");
-        } catch (Exception e) {
-            System.out.println("Error creating tables in the database: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
 }
+//    @PersistenceContext
+//    private EntityManager entityManager;
+//
+//    @Transactional
+//    public void initDb() {
+//        try {
+//            String createQuotesTableQuery = "DROP TABLE IF EXISTS Quotes2;" +
+//                    "CREATE TABLE IF NOT EXISTS Quotes2 (" +
+//                    "id SERIAL PRIMARY KEY," +
+//                    "text TEXT NOT NULL," +
+//                    "quoteid INT NOT NULL" +
+//                    ");";
+//
+//            String createChatsTableQuery = "DROP TABLE IF EXISTS Chats2;" +
+//                    "CREATE TABLE IF NOT EXISTS Chats2 (" +
+//                    "id BIGSERIAL PRIMARY KEY," +
+//                    "chatId BIGINT NOT NULL," +
+//                    "lastId INT NOT NULL" +
+//                    ");";
+//
+//            entityManager.createNativeQuery(createQuotesTableQuery).executeUpdate();
+//            entityManager.createNativeQuery(createChatsTableQuery).executeUpdate();
+//            // Добавление других таблиц
+//
+//            System.out.println("Tables 'Quotes2' and 'Chats2' created successfully.");
+//        } catch (Exception e) {
+//            System.out.println("Error creating tables in the database: " + e.getMessage());
+//            e.printStackTrace();
+//        }
+//    }
+//}
